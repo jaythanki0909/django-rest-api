97ba931e0532ef3797c4d0fe7355e330b115173e
Jay <thankijay20@gmail.com>
2024-10-23 10:08:37 +0530

    added profile feed api and permissions

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -5,6 +5,7 @@ from profiles_api import views
 router = DefaultRouter()
 router.register('hello2',views.HelloViewSet,basename='hello-viewset')
 router.register('profile',views.UserProfileViewSet)
+router.register('feed',views.UserProfileFeedViewSet)
 urlpatterns = [
     path('hello/',views.HelloApiView.as_view()),
     path('login/',views.UserLoginAPIView.as_view()),
--- profiles_api/views.py
+++ profiles_api/views.py
@@ -8,6 +8,7 @@ from profiles_api import permissions
 from rest_framework import filters
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.settings import api_settings
+from rest_framework.permissions import IsAuthenticatedOrReadOnly,IsAuthenticated
 
 class HelloApiView(APIView):
     """Test API View"""
@@ -64,3 +65,15 @@ class UserProfileViewSet(viewsets.ModelViewSet) :
 
 class UserLoginAPIView(ObtainAuthToken) :
     renderer_classes= api_settings.DEFAULT_RENDERER_CLASSES
+
+class UserProfileFeedViewSet(viewsets.ModelViewSet) :
+    authentication_classes= (TokenAuthentication,)
+    permission_classes = (
+        permissions.UpdateOwnStatus,
+        IsAuthenticated,
+        )
+    serializer_class = serializers.ProfileFeedItemSerializer
+    queryset = models.ProfileFeedItem.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save(user_profile=self.request.user)
